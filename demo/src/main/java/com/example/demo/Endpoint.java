53991776ef97f4b6fabb79a3ed9ead6b78325bd7
raja <narasimmaraja.bs@gmail.com>
2023-12-22 19:17:45 +0530

    init

---
+++ demo/src/main/java/com/example/demo/Endpoint.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Endpoint {
+	@PostMapping("/raja")
+	public int sum(@RequestParam int x,@RequestParam int y) {
+		int sum=x+y;
+		return sum;
+	
+	}
+
+}
